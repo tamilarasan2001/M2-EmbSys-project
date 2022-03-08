3c14163c6d0121d70f72aa7ee9d823d9f5970ac8
tamilarasan2001 <98837668+tamilarasan2001@users.noreply.github.com>
2022-03-08 10:58:14 +0530

    Add files via upload

---
+++ Project/3_Implementation/Code/solartracker.ino
@@ -0,0 +1,88 @@
+#include <Servo.h>
+//defining Servos
+Servo servo1;
+int servo = 0;
+int servoLimitHigh = 150;
+int servoLimitLow = 10;
+
+Servo servo2; 
+int servov = 0; 
+int servovLimitHigh = 150;
+int servovLimitLow = 10;
+//Assigning LDRs
+int ldrtopleft = 2; //top left LDR green
+int ldrtopright = 1; //top right LDR yellow
+int ldrbottomleft = 3; // bottom left LDR blue
+int ldrbottomright = 0; // bottom right LDR orange
+
+ void setup () 
+ {
+  servo1.attach(10);
+  servo1.write(0);
+  servo2.attach(9);
+  servo2.write(0);
+  delay(500);
+ }
+
+void loop()
+{
+  servo = servo1.read();
+  servov = servo2.read();
+  //capturing analog values of each LDR
+  int topl = analogRead(ldrtopleft);
+  int topr = analogRead(ldrtopright);
+  int botl = analogRead(ldrbottomleft);
+  int botr = analogRead(ldrbottomright);
+  // calculating average
+  int averagetop = (topl + topr) / 2; //average of top LDRs
+  int averagebottom = (botl + botr) / 2; //average of bottom LDRs
+  int averageleft = (topl + botl) / 2; //average of left LDRs
+  int averageright = (topr + botr) / 2; //average of right LDRs
+
+  if (averagetop < averagebottom)
+  {
+    servo2.write(servov +1);
+    if (servov > servovLimitHigh) 
+     { 
+      servov = servovLimitHigh;
+     }
+    delay(10);
+  }
+  else if (averagebottom < averagetop)
+  {
+    servo2.write(servov -1);
+    if (servov < servovLimitLow)
+  {
+    servov = servovLimitLow;
+  }
+    delay(10);
+  }
+  else 
+  {
+    servo2.write(servov);
+  }
+  
+  if (averageleft > averageright)
+  {
+    servo1.write(servo +1);
+    if (servo > servoLimitHigh)
+    {
+    servo = servoLimitHigh;
+    }
+    delay(10);
+  }
+  else if (averageright > averageleft)
+  {
+    servo1.write(servo -1);
+    if (servo < servoLimitLow)
+     {
+     servo = servoLimitLow;
+     }
+    delay(10);
+  }
+  else 
+  {
+    servo1.write(servo);
+  }
+  delay(50);
+}
